5852b9de779a0a291906e0cda1901060da7e6e0b
Lucas Combs <l-combs@hotmail.com>
2019-06-29 15:43:47 -0400

    Implemented base functionality!

---
+++ ProgramStarter.py
@@ -0,0 +1,32 @@
+import psutil
+import subprocess
+import json
+import os.path
+
+json_tasks = json.loads(open("tasks.json", "r").read())
+
+for task in json_tasks["Tasks"]:
+    path = task["path"]
+    exe = task["exe"]
+    args = task["args"]
+    name = task["name"]
+
+    # Build system argument list
+    cmd = list()
+    cmd.append(os.path.join(path, exe))
+    cmd.extend(args)
+
+    running_already = False
+
+    # Determine if this program is already running
+    for running_process in psutil.process_iter():
+        if running_process.name().casefold() == exe.casefold():
+            running_already = True
+            break
+
+    if not running_already:
+        try:
+            pid = subprocess.Popen(cmd, creationflags=subprocess.CREATE_NEW_CONSOLE).pid
+            print("Started", name, "with pid:", pid)
+        except (SystemError, FileNotFoundError):
+            print("Had an issue opening", name)
